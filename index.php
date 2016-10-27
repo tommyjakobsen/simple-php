c693130fb5ccbca4294d219b76c53fa6d970ebde
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:07:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<html><head><title>Simple index page</title></head><body>";
+
+
+echo "<br><font size='12' color='green'><BR>This is a test page...</font>";
+
+
+echo "</body></html>";
+
+
+?>
