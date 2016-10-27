c5e5cd4baec88fdc6bae9a9b8ebe27565e1391d2
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:15:34 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ echo "<head><title>Test for OpenShift</title></head>";
 echo "<body bgcolor='#abcdef'>";
 
 
-echo "<font size=14 color='black'><br><br>Test website...</font>";
+echo "<font size=14 color='black'><br><br>Test website...</font><br><br>";
 
+print_r($_SERVER);
 echo "</body></html>";
