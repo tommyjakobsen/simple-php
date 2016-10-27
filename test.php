f5968e9cbf91535e80ee1df8310cd468e485a666
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 13:03:16 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<head><title>Test for OpenShift</title></head>";
+echo "<body bgcolor='#abcdef'>";
+
+
+echo "<font size=14 color='black'><br><br>Test website...</font>";
+
+echo "</body></html>";
