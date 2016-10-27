8fde193294e3250fff7e06e6e84954c2ba4688d0
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:31:01 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,11 +4,8 @@ echo "<head><title>Test for OpenShift</title></head>";
 echo "<body bgcolor='#abcdef'>";
 
 
-echo "<font size=14 color='black'><br><br>Test website...</font><br><br>";
+echo "<center><font size=14 color='black'><br><br>OpenShift Test website...</font><br><br>";
 
-//print_r($_SERVER);
-foreach($_SERVER as $key=>$val)
-{
- echo "$key: $val<br>"; 
-}
+echo "You are now accessing <b><font color='red'>$_SERVER[REMOTE_ADDR]</font></b></center>";
+
 echo "</body></html>";
