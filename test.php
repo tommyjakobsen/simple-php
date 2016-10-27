92cf8d21caabdef05ff4f04e4a9d64d96ea408d3
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:45:22 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,9 +3,27 @@
 echo "<head><title>Test for OpenShift</title></head>";
 echo "<body bgcolor='#abcdef'>";
 
+if ( isset( $_GET['server'] )) {
+  $server=$_GET['server']; 
+}else{
+  $server="";
+}
 
-echo "<center><font size=14 color='black'><br><br>OpenShift Test website...</font><br><br>";
-
-echo "You are now accessing <b><font color='red'>$_SERVER[REMOTE_ADDR]</font></b></center>";
-
+(if $server="")
+{
+ echo "<center><font size=11 color='black'><br><br>Welcome to OpenShift Test website...<b>Active Server is: $_SERVER[SERVER_ADDR]</font><br><br>
+ $server="$_SERVER[SERVER_ADDR]";
+ echo "<META HTTP-EQUIV='refresh' content='5;URL=./test.php?server=$server'>";
+}else{
+ if ($server==$_SERVER[SERVER_ADDR]")
+ {
+   echo "<center><font size=11 color='black'><br><br>You are accessing server: $_SERVER[SERVER_ADDR]</font>";
+   
+ }else{
+  echo "<center><font size=11 color='black'><br><br>You have now changed from server: $server to server: $_SERVER[SERVER_ADDR]</font>";
+   $server="$_SERVER[SERVER_ADDR]";
+   echo "<META HTTP-EQUIV='refresh' content='30;URL=./test.php?server=$server'>";
+ }
+}
+ 
 echo "</body></html>";
