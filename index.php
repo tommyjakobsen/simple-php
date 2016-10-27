ea4ade75884e6fcfd39ab92d3739b556b021276e
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:08:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo "<html><head><title>Simple index page</title></head><body>";
 
 
 echo "<br><font size='12' color='green'><BR>This is a test page...</font>";
-
+echo "<a href='./test.php'><b>[ Test php page ]</b></a>";
 
 echo "</body></html>";
 
