34a1bbc1d4f53a433e2f7f6540291b3c9deb6326
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:18:41 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,5 +6,9 @@ echo "<body bgcolor='#abcdef'>";
 
 echo "<font size=14 color='black'><br><br>Test website...</font><br><br>";
 
-print_r($_SERVER);
+//print_r($_SERVER);
+foreach($_SERVER as $key=>$val)
+{
+ echo "$key: $val<br>"; 
+}
 echo "</body></html>";
