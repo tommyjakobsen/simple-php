7d2e6079eb86129e1cd5a6354a41234211ebe621
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-27 14:49:55 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,21 @@
 <?php
-
 echo "<head><title>Test for OpenShift</title></head>";
 echo "<body bgcolor='#abcdef'>";
 
-if ( isset( $_GET['server'] )) {
+if (isset($_GET['server']))
+{
   $server=$_GET['server']; 
 }else{
   $server="";
 }
 
-(if $server == "")
+if ($server == "")
 {
- echo "<center><font size=11 color='black'><br><br>Welcome to OpenShift Test website...<b>Active Server is: $_SERVER[SERVER_ADDR]</font><br><br>
- $server="$_SERVER[SERVER_ADDR]";
+ echo "<center><font size=11 color='black'><br><br>Welcome to OpenShift Test website...<b>Active Server is: $_SERVER[SERVER_ADDR]</font><br><br>";
+$server="$_SERVER[SERVER_ADDR]";
  echo "<META HTTP-EQUIV='refresh' content='5;URL=./test.php?server=$server'>";
 }else{
- if ($server == $_SERVER[SERVER_ADDR]")
+ if ($server == $_SERVER[SERVER_ADDR])
  {
    echo "<center><font size=11 color='black'><br><br>You are accessing server: $_SERVER[SERVER_ADDR]</font>";
    echo "<META HTTP-EQUIV='refresh' content='5;URL=./test.php?server=$server'>";
