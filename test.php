eaf656b263255cfd46268c21be505e297d708e1e
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-10-28 10:15:03 +0200

    Update test.php

--- test.php
+++ test.php
@@ -13,16 +13,16 @@ if ($server == "")
 {
  echo "<center><font size=11 color='black'><br><br>Welcome to OpenShift Test website...<b><br>Active Server is: $_SERVER[SERVER_ADDR]</font><br><br>";
 $server="$_SERVER[SERVER_ADDR]";
- echo "<META HTTP-EQUIV='refresh' content='5;URL=./test.php?server=$server'>";
+ echo "<META HTTP-EQUIV='refresh' content='2;URL=./test.php?server=$server'>";
 }else{
  if ($server == $_SERVER[SERVER_ADDR])
  {
    echo "<center><font size=11 color='black'><br><br>You are accessing server: $_SERVER[SERVER_ADDR]</font>";
-   echo "<META HTTP-EQUIV='refresh' content='5;URL=./test.php?server=$server'>";
+   echo "<META HTTP-EQUIV='refresh' content='2;URL=./test.php?server=$server'>";
  }else{
   echo "<center><font size=11 color='black'><br><br>You have now changed from server: $server to server: $_SERVER[SERVER_ADDR]</font>";
    $server="$_SERVER[SERVER_ADDR]";
-   echo "<META HTTP-EQUIV='refresh' content='30;URL=./test.php?server=$server'>";
+   echo "<META HTTP-EQUIV='refresh' content='2;URL=./test.php?server=$server'>";
  }
 }
  
