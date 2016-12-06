200600fff666e4b6cdb1cd1ee70fe8376c413afa
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-12-06 18:27:20 +0100

    Update img.php

--- img.php
+++ img.php
@@ -5,47 +5,45 @@ if(isset($_GET["pod"]))
 }else{
     $pod="1";
 }
-
 $target_file="server.txt";
 $target_file2="server2.txt";
-
-
 $server1 = file_get_contents("$target_file");
 $server2 = file_get_contents("$target_file2");
-
 //
 $activepod= imagecreatefrompng("img/activePod.png");
 $deadpod= imagecreatefrompng("img/deadPod.png");
-
 $host=php_uname('n');
 $image = imagecreatefrompng("img/openshift_total.png");
-
-
 //NUMBER OF PODS
- imagestring($image, 130, 10, 110,  "Running Pod(s): $pod", $text_color);
+ imagestring($image, 100, 10, 60,  "Running Pod(s): $pod", $text_color);
 //Create active pod and connection
-if($server1 == $server2 || !preg_match('/[0-9]/', $server2)  || (!preg_match('/[0-9]/', $server2) && !preg_match('/[0-9]/', $server1)))
-	{
-	imageline($image, 400, 75, 350, 230, $blue);
-	imagecopy($image, $activepod, 320, 230, 0,0,40,44);
-	}else{
-
-        imagecopy($image, $deadpod, 320, 230, 0,0,40,44);
-
-	//New server	
-	imageline($image, 400, 75, 430, 233, $blue);
-        imagecopy($image, $activepod, 420, 230, 0,0,40,44);
+if($pod == 1)
+        {
+        imageline($image, 400, 75, 350, 230, $blue);
+        imagecopy($image, $activepod, 320, 230, 0,0,40,44);
         }
 
+if($pod  == 2)
+        {
+        imageline($image, 400, 75, 430, 233, $blue);
+        imagecopy($image, $activepod, 420, 230, 0,0,40,44);
+        imageline($image, 400, 75, 350, 230, $blue);
+        imagecopy($image, $activepod, 320, 230, 0,0,40,44);
+        }
 
+if($pod  == 3)
+        {
+        imageline($image, 400, 75, 430, 233, $blue);
+        imagecopy($image, $activepod, 420, 230, 0,0,40,44);
+        imageline($image, 400, 75, 350, 230, $blue);
+        imagecopy($image, $activepod, 320, 230, 0,0,40,44);
+        imageline($image, 400, 75, 550, 230, $blue);
+        imagecopy($image, $activepod, 550, 230, 0,0,40,44);
+        }
 header("Content-type: image/jpeg");
    
-
 imageJpeg($image);
 imageDestroy($image);
-
 imageDestroy($activepod);
 imageDestroy($deadpod);
-
-
 ?>
