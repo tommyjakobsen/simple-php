639915127a93df58b1ce9af143574e2899956fae
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-12-06 18:32:47 +0100

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 $target_file="server.txt";
 $target_file2="server2.txt";
 echo "<head><title>Test for OpenShift</title></head>";
-echo "<body bgcolor='#abcdef'>";
+echo "<body bgcolor='#ffccbb'>";
 
 
 $server1 = file_get_contents("$target_file");
