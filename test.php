ce35e6af77fdaf9059705dc342c31ded3977c35a
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-12-08 15:06:33 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
-
+flush();
+ob_flush();
 $target_file="server.txt";
 $target_file2="server2.txt";
-ob_end_clean();
-ob_flush();
+
 echo "<head><title>Test for OpenShift</title></head>";
 echo "<body bgcolor='#abcdef'>";
 
@@ -13,16 +13,21 @@ $server="$_SERVER[SERVER_ADDR]";
 
 if(!preg_match("/$server/", $server1))
         {
+        flush();
         ob_flush();
         echo "<center><font size=3 color='red'><br><br>You have now changed pod: $server</font>";
-        ob_end_flush();
-        ob_end_clean();
+         flush();
+        ob_flush();
         sleep(4);
         $handle2=fopen("$target_file2", "w");
         fwrite($handle2, $server);
         fclose($handle2);
         }else{
+        flush();
+        ob_flush();
         echo "<center><font size=3 color='black'><br><br>You are accessing pod<br>with internal IP: $server</font>";
+        flush();
+        ob_flush();
         }
 
 $handle=fopen("$target_file", "w");
