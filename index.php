94bb09027f647fc6a7668dae99d3e783c5e43a4c
tommyjakobsen <tommy.jakobsen@outlook.com>
2016-12-08 12:42:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
 
 <script>
 var to = 2;
-
 function gogo(){
 var d=new Date(),
     dummy=d.getTime(),
@@ -30,14 +29,13 @@ else{
 var obj = pix[i],
     s_rc=obj.getAttribute('src'),
     pure_src = s_rc.substring(0,s_rc.indexOf('x=x')+3);
-obj.setAttribute('src',pure_src+'&rand='+dummy);
-obj.setAttribute('title',pure_src+'&rand='+dummy);
+obj.setAttribute('src',pure_src+'&'+dummy);
+obj.setAttribute('title',pure_src+'&'+dummy);
 obj.nextSibling.innerHTML=obj.getAttribute('src');
 }
 }
 setTimeout(gogo,to*500);
 }
-
 onload=gogo;
 </script>
 
@@ -58,7 +56,7 @@ var iframe = document.getElementById('myframe2');
 iframe.src = iframe.src;
 //alert(iframe.src);
 }
-setInterval(refreshIframe, 3000);
+setInterval(refreshIframe, 6000);
 </script>
 
 
@@ -71,12 +69,17 @@ setInterval(refreshIframe, 3000);
 <tr><td align=middle></td></tr>
 <?php
     
-
+if(isset($_GET["pod"]))
+{
+    $pod=$_GET["pod"];
+}else{
+    $pod="2";
+}
 $host=$_SERVER["HTTP_HOST"];
-echo "<tr><td align=middle><iframe src=\"test.php\" frameBorder=\"1\" scrolling=\"no\" id='myframe' width=270></iframe> <br><img src='./img/keyboard.png'> </td></tr>";
+echo "<tr><td align=middle><iframe src=\"http://$host/test.php?rand=".rand(0,1000)."\&pod=$pod\" frameBorder=\"1\" scrolling=\"no\" id='myframe' width=270></iframe> <br><img src='./img/keyboard.png'> </td></tr>";
 echo "<tr><td align=middle><img src='./img/leftArrow.png' height=50></td></tr>
 <tr><td colspan=2 cellspan=2 align=middle>
-<img src=\"img.php\" class=\"refr\" alt=\"\" />
+<img src=\"./img.php?x=x&pod=$pod\" class=\"refr\" alt=\"\" />
 </td></tr></table>";
 ?>
 </body>
